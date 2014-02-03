852c63b1a3496029d92872854acd8da980494a8f
amaia <alexandre.maia@gmail.com>
2014-02-03 10:47:34 +0000

    initial

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "security"
+maintainer        ""
+maintainer_email  ""
+description       "Basic security for ubuntu"
+version           "0.0.1"
+
+recipe "security", "Locks down a basic Ubuntu Install"
+
+supports "ubuntu"
